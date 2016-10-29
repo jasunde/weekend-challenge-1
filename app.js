8416a5182542763893e489ed8e6c54617b9c215d
jasunde <jlaurits@gmail.com>
2016-10-29 12:45:38 -0500

    Remove deleted employee from employeeList. Change monthly salary total.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var employeeList = [],
       $employeeTable = $('#employeeTable'),
       $employeeForm = $('#employeeForm'),
+      $monthlyPayroll = $('#monthlyPayroll'),
       $firstInput = $employeeForm.find('input').first(),
       monthlyPayroll,
       employeeCounter = 0;
@@ -9,6 +10,8 @@ $(document).ready(function() {
   // Focus on first input after DOM ready
   $firstInput.focus();
 
+
+  // Add employee to DOM and employeeList
   $employeeForm.on('submit', function (event) {
     event.preventDefault();
 
@@ -27,7 +30,7 @@ $(document).ready(function() {
     });
 
     // Add unique identifier to employee
-    employee.employeeCounter = employeeCounter;
+    employee.employeeCounter = 'e' + employeeCounter;
     employeeCounter++;
 
     // Add employee object to employeeList
@@ -36,20 +39,28 @@ $(document).ready(function() {
     // Add a new row to table with employee data
     addEmployeeRow(employee, $employeeTable);
 
-    // Calculate the monthly payroll total for all employees
-    monthlyPayroll = calculateMonthlyPayroll(employeeList);
-
-    // Change monthly salary expenditures total on page
-    $('#monthlyPayroll').text(monthlyPayroll.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
+    adjustMonthlyPayroll(employeeList, $monthlyPayroll);
   });
 
+
+  // Remove employee from DOM and employeeList
   $employeeTable.on('click', '.delete', function () {
-    $(this).closest('tr').remove();
-  })
+    var $row = $(this).closest('tr');
+
+    employeeList = employeeList.filter(function (employee) {
+      return employee.employeeCounter !== $row.data('employeeCounter');
+    });
+
+    $row.remove();
+
+    adjustMonthlyPayroll(employeeList, $monthlyPayroll);
+  });
 });
 
+// Object, jQuery Table -> void
+// Append tr with cells of supplied employee to supplied table
 function addEmployeeRow(employee, $table) {
-  var $tr = $('<tr></tr>').attr('id', 'e' + employee.employeeCounter);
+  var $tr = $('<tr></tr>').data('employeeCounter', employee.employeeCounter);
   addCell(employee.firstName, $tr);
   addCell(employee.lastName, $tr);
   addCell(employee.idNumber, $tr);
@@ -59,12 +70,26 @@ function addEmployeeRow(employee, $table) {
   $table.append($tr);
 }
 
+// Variable, jQuery Table Row -> void
+// Append new td with given data to given tr
 function addCell(data, $row) {
   $row.append($('<td>' + data + '</td>'));
 }
 
+// Array of Employee objects -> Number
+// Calculate the monthly payroll expense for the given list of employees
 function calculateMonthlyPayroll(employees) {
   return employees.reduce(function (total, employee) {
     return total += employee.annualSalary / 12;
   }, 0);
+}
+
+// Array of Employee Objects, jQuery HTMLElement -> void
+// Change the monthly payroll total according to the given list of employees
+function adjustMonthlyPayroll(employees, $total) {
+    // Calculate the monthly payroll total for all employees
+    monthlyPayroll = calculateMonthlyPayroll(employees);
+
+    // Change monthly salary expenditures total on page
+    $total.text(monthlyPayroll.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
 }
