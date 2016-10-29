d0cb4f6d4041abd4785fa06c1fc80e4cf1ba0ef3
jasunde <jlaurits@gmail.com>
2016-10-29 12:02:45 -0500

    Added button to delete employee row

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
       $employeeTable = $('#employeeTable'),
       $employeeForm = $('#employeeForm'),
       $firstInput = $employeeForm.find('input').first(),
-      monthlyPayroll;
+      monthlyPayroll,
+      employeeCounter = 0;
 
   // Focus on first input after DOM ready
   $firstInput.focus();
@@ -20,26 +21,41 @@ $(document).ready(function() {
     // Return focus to first input of form
     $firstInput.focus();
 
+    // Convert form data to employee object
     fields.forEach(function (field){
       employee[field.name] = field.value;
     });
 
+    // Add unique identifier to employee
+    employee.employeeCounter = employeeCounter;
+    employeeCounter++;
+
+    // Add employee object to employeeList
     employeeList.push(employee);
 
+    // Add a new row to table with employee data
     addEmployeeRow(employee, $employeeTable);
 
+    // Calculate the monthly payroll total for all employees
     monthlyPayroll = calculateMonthlyPayroll(employeeList);
+
+    // Change monthly salary expenditures total on page
     $('#monthlyPayroll').text(monthlyPayroll.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
   });
+
+  $employeeTable.on('click', '.delete', function () {
+    $(this).closest('tr').remove();
+  })
 });
 
 function addEmployeeRow(employee, $table) {
-  var $tr = $('<tr></tr>').attr('id', 'e' + employee.idNumber);
+  var $tr = $('<tr></tr>').attr('id', 'e' + employee.employeeCounter);
   addCell(employee.firstName, $tr);
   addCell(employee.lastName, $tr);
   addCell(employee.idNumber, $tr);
   addCell(employee.jobTitle, $tr);
   addCell(employee.annualSalary, $tr);
+  $tr.append('<td><button class="delete">Delete</button></td>');
   $table.append($tr);
 }
 
